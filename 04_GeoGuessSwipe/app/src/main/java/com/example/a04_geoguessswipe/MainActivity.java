c34f804b7aab2f01c278d77fd9f6d357a9d5e019
Faraz Foroozan <farazforoozan@gmail.com>
2019-02-25 13:30:09 +0100

    04_GeoGuessSwipe moet alleen nog de koppeling tussen de swipe en de yes/no implementeren

--- 04_GeoGuessSwipe/app/src/main/java/com/example/a04_geoguessswipe/MainActivity.java
+++ 04_GeoGuessSwipe/app/src/main/java/com/example/a04_geoguessswipe/MainActivity.java
@@ -1,11 +1,15 @@
-package com.example.a03_placesofinterest;
+package com.example.a04_geoguessswipe;
 
 import android.support.annotation.NonNull;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,6 +20,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RecyclerView.OnItemTouchListener {
 
+    private static final String DEBUG_TAG = "MyActivity";
     List<GeoObject> mGeoObjects;
     RecyclerView mGeoRecyclerView;
     private GestureDetector mGestureDetector;
@@ -30,16 +35,18 @@ public class MainActivity extends AppCompatActivity implements RecyclerView.OnIt
 
         for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
             mGeoObjects.add(new GeoObject(GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
-                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i]));
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i],
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_INEUROPE[i]));
         }
 
-        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, LinearLayoutManager.VERTICAL);
 
         mGeoRecyclerView.setLayoutManager(mLayoutManager);
         mGeoRecyclerView.setHasFixedSize(true);
-        mGeoRecyclerView.setHasFixedSize(true);
-        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+
+        final GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
         mGeoRecyclerView.setAdapter(mAdapter);
+
         mGeoRecyclerView.addOnItemTouchListener(this);
 
         mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
@@ -48,8 +55,29 @@ public class MainActivity extends AppCompatActivity implements RecyclerView.OnIt
                 return true;
             }
         });
-    }
 
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    //Called when a user swipes left or right on a ViewHolder
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+                        //Get the index corresponding to the selected position
+                        int position = (viewHolder.getAdapterPosition());
+                        Toast.makeText(MainActivity.this, swipeDir + "", Toast.LENGTH_SHORT).show();
+                        //mReminders.remove(position);
+                        //mAdapter.notifyItemRemoved(position);
+                    }
+                };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mGeoRecyclerView);
+    }
     @Override
     public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
         View child = rv.findChildViewUnder(e.getX(), e.getY());
@@ -62,8 +90,8 @@ public class MainActivity extends AppCompatActivity implements RecyclerView.OnIt
 
     @Override
     public void onTouchEvent(@NonNull RecyclerView recyclerView, @NonNull MotionEvent motionEvent) {
-
     }
+
 
     @Override
     public void onRequestDisallowInterceptTouchEvent(boolean b) {
