fc94dae14978b61be2bf7bdf072e0903ca4e457a
Faraz Foroozan <farazforoozan@gmail.com>
2019-04-24 20:24:26 +0200

    Start MovieDB project

---
+++ TheMovieDB/app/src/main/java/com/example/themoviedb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.themoviedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//URL: http://api.themoviedb.org/3/movie/popular?api_key=38509682663bd155406c4977e737b29e
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
